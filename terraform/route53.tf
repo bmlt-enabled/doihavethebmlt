a21cb7032c698e3a25add129e3a1804ecd30c665
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2020-08-20 11:39:46 -0400

    migrate dns to bmlt-enabled, remove unused resources

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,14 +1,14 @@
-// TODO: import zone if possible
-data aws_route53_zone domain {
-  name = "doihavethebmlt.org."
+resource aws_route53_zone domain {
+  name = "doihavethebmlt.org"
 }
 
 resource aws_route53_record cdn_cname_naked {
-  zone_id = data.aws_route53_zone.domain.id
-  name    = "doihavethebmlt.org"
+  zone_id = aws_route53_zone.domain.id
+  name    = aws_route53_zone.domain.name
   type    = "A"
   ttl     = 300
-  records = ["185.199.108.153",
+  records = [
+    "185.199.108.153",
     "185.199.109.153",
     "185.199.110.153",
     "185.199.111.153",
@@ -16,11 +16,12 @@ resource aws_route53_record cdn_cname_naked {
 }
 
 resource aws_route53_record cdn_cname_www {
-  zone_id = data.aws_route53_zone.domain.id
-  name    = "www.doihavethebmlt.org"
+  zone_id = aws_route53_zone.domain.id
+  name    = "www.${aws_route53_zone.domain.name}"
   type    = "A"
   ttl     = 300
-  records = ["185.199.108.153",
+  records = [
+    "185.199.108.153",
     "185.199.109.153",
     "185.199.110.153",
     "185.199.111.153",
