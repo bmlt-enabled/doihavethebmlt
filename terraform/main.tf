7b692d54136953e578bc1d14399395364944e47c
Danny <danny.gershman@gmail.com>
2020-08-20 11:29:22 -0400

    migration to gh-pages

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  backend "s3" {
+  backend s3 {
     bucket  = "dihtbmlt-tfstate"
     key     = "state"
     region  = "us-east-1"
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-provider "aws" {
+provider aws {
   region  = "us-east-1"
   profile = "personal"
 }
