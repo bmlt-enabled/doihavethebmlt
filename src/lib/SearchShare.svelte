523acb06511be6b5b124808f5ebefacb21b0074c
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2021-10-15 20:52:31 -0400

    dark mode

--- src/lib/SearchShare.svelte
+++ src/lib/SearchShare.svelte
@@ -20,17 +20,17 @@
   <h4>Share This Search</h4>
   <div class="field has-addons">
     <div class="control is-expanded">
-      <input class="input is-light" type="text" name="url" value={url} readonly />
+      <input class="input" type="text" name="url" value={url} readonly />
     </div>
     <div class="control">
-      <button class="button is-light" on:click={handleCopy}>
+      <button class="button" on:click={handleCopy}>
         {#if copied === true}
           <span class="icon is-small">
-            <i class="fas fa-copy" />
+            <i class="far fa-copy" />
           </span>
         {:else}
           <span class="icon is-small">
-            <i class="far fa-copy" />
+            <i class="fas fa-copy" />
           </span>
         {/if}
         <span>Copy</span>
