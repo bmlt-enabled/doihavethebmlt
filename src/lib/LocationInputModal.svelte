b98d2200de26329bf92ef538d296dc6ec2f39a77
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-03-19 21:28:13 -0400

    use async functions

--- src/lib/LocationInputModal.svelte
+++ src/lib/LocationInputModal.svelte
@@ -13,23 +13,21 @@
     }
   }
 
-  function handleSearch() {
+  async function handleSearch() {
     if (value) {
-      let url = `https://api.geocod.io/v1.6/geocode?api_key=8a8ceea13e3cad86e8c64616d41d3333d488c66&q=${value}`
-      fetch(url)
-        .then(response => response.json())
-        .then(response => response.results[0])
-        .then(result => {
-          onSearch({
-            coords: {
-              latitude: result.location.lat,
-              longitude: result.location.lng
-            }
-          });
-        })
-        .catch(error => {
-          onError(error);
+      try {
+        let url = `https://api.geocod.io/v1.6/geocode?api_key=8a8ceea13e3cad86e8c64616d41d3333d488c66&q=${value}`;
+        let response = await fetch(url);
+        let result = response.results[0];
+        onSearch({
+          coords: {
+            latitude: result.location.lat,
+            longitude: result.location.lng
+          }
         });
+      } catch (error) {
+        onError(error);
+      }
     }
   }
 
@@ -43,7 +41,7 @@
   <div class="modal-content p-5">
     <div class="field has-addons">
       <div class="control is-expanded">
-        <input class="input is-light" type="text" bind:value placeholder="Enter City/State or Zip Code"/>
+        <input class="input is-light" type="text" bind:value placeholder="Enter City/State or Zip Code" />
       </div>
       <div class="control">
         <button class="button is-light" on:click={handleSearch}>Search</button>
