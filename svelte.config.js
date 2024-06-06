f53023940386d6425ac5dece3a94e9c1ab8b1862
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2024-06-06 14:11:08 -0400

    migrate to svlete 2, ts (#16)

--- svelte.config.js
+++ svelte.config.js
@@ -3,32 +3,19 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-
-    adapter: adapter({
-      fallback: 'index.html'
-    }),
-
-    vite: {
-      css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: '@import "src/variables.scss";'
-          }
+    preprocess: preprocess({
+        scss: {
+            prependData: "@import './src/variables.scss';"
         }
-      }
+    }),
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
+            precompress: true
+        })
     }
-  },
-
-  preprocess: [
-    preprocess({
-      scss: {
-        prependData: '@import "src/variables.scss";'
-      }
-    })
-  ]
 };
 
 export default config;
